9c32c28edc2be4a84cc3b49dff9c02957b399a91
Gaurav Kaushalye <gauravkaushalye@gmail.com>
2026-03-09 22:25:22 +0530

    yep

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 3006;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
-  res.end("chicken!\n");
+  res.end("cicd flow test!\n");
 });
 
 server.listen(port, hostname, () => {
